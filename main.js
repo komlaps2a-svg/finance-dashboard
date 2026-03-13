e8a75df1261eadef2242d79f13c38ae7307956db
engyourbae <komlaps2a@gmail.com>
2026-03-13 13:12:35 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==========================================
 // 1. SISTEM AUTO-UPDATE & SMART CACHE BUSTER
 // ==========================================
-const APP_VERSION = '29.2'; 
+const APP_VERSION = '29.3'; 
 
 function checkAppVersion() {
     const savedVersion = localStorage.getItem('finance_app_version');
